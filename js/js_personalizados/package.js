21b60f0689fbecbf129371dd8acf2b775f621422
lilicolls <lilianacolls@gmail.com>
2018-04-05 00:15:31 -0400

    agregando metodo para buscar un paquete por su id, y la ubicacion del camion en el que es transportado

---
+++ js/js_personalizados/package.js
@@ -0,0 +1,31 @@
+'use strict'
+
+
+function findPackage(idPackage = "62 27 F2 8B") {
+    //metodo para filtrar un paquete por su id!
+
+    database.ref('/paquetes/' + idPackage).once('value')
+    .then((snapshot)=>{
+        if(!snapshot.val()){
+            console.log("el paquete no existe")
+        }else{
+            //si el paquete existe obtengo los datos que quiera 
+            // y busco la ubicacion del camion en el que esta
+            const unidadTransp = snapshot.val().unidad
+            console.log(unidadTransp) 
+            database.ref('/unidad/' + unidadTransp).once('value')
+            .then((snapshot)=>{
+                if(!snapshot.val()){
+                    console.log("el camion no existe:(")
+                }else {
+                    console.log( snapshot.val())
+                    // llamo a la funcion que imprima los datos en texto mas el mapa
+                }
+            })
+        }
+    })
+    .catch((err)=>{
+        console.log(`Ocurrió un error en la consulta ${err}`) 
+    })
+
+}
