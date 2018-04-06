797c1097a53673b28edd97aa6c52da1f9bc5dbce
lilicolls <lilianacolls@gmail.com>
2018-04-06 12:06:26 -0400

    agregando mapa de google maps para mostrar la ubicacion del paquete

--- js/js_personalizados/utilities.js
+++ js/js_personalizados/utilities.js
@@ -31,3 +31,18 @@ function fillTable (obj){
       })
       $("#tabla").tabulator("setData", arr);   
   }
+
+
+
+function initMap() {
+   console.log("hola")
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
